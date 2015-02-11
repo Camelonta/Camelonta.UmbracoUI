2d3c25d35bf0bc1fb146d109023567735468f426
sebastian <sebastian@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-02-11 14:31:17 +0000

    Umbraco Plugins - UI och ActivityLog

---
+++ camelontacms.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    setTimeout(loadCamelontaLogo, 300);
+});
+
+
+var camelontaLogoLoaded = false;
+
+function loadCamelontaLogo() {
+
+    if ($("#applications .sections").length) {
+
+        $("#applications .sections").prepend("<li class='camelonta-logo'><a href='/umbraco'><img src='../App_Plugins/Camelonta.UI/camelontacms.png'/></a></li>");
+
+        $("li.avatar").detach().insertBefore('li.help');
+
+        camelontaLogoLoaded = true;
+    }
+
+    if (!camelontaLogoLoaded)
+        setTimeout(loadCamelontaLogo, 300);
+}
