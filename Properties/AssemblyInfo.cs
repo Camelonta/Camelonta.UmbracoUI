04dea47a43eb87238ec701396cb6eaadba473ebf
sebastian <sebastian@d252d4b6-3862-8246-84e1-cf8c128d3774>
2016-01-28 14:02:12 +0000

    Camelonta.UI - Uppdaterade UI för Umbraco 7.4, gjorde om all javascript så att allt laddas via angular. Minskade ner koden rejält och borde fungera mycket bättre

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Camelonta.UI")]
 [assembly: AssemblyProduct("Camelonta.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.5.0")]
-[assembly: AssemblyFileVersion("2.0.5.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
